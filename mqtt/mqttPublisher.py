1108aa949c913604a1082f9a262289d8499f3b6f
Evan Carlin <evan@carlin.com>
2017-01-18 10:24:48 -0800

    Pep-8 styling correction

--- mqtt/mqttPublisher.py
+++ mqtt/mqttPublisher.py
@@ -4,6 +4,7 @@
 import paho.mqtt.client as mqtt
 import time
 
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -11,6 +12,7 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("mqtt")
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -23,6 +25,6 @@ client.on_message = on_message
 client.connect("localhost", 1883, 60)
 
 for x in range(0, 100):
-	print("Sending a message")
-	client.publish(topic="mqtt", payload="Hello", qos=0, retain=False)
-	time.sleep(1)
+    print("Sending a message")
+    client.publish(topic="mqtt", payload="Hello", qos=0, retain=False)
+    time.sleep(1)
--- mqtt/mqttSubscriber.py
+++ mqtt/mqttSubscriber.py
@@ -3,6 +3,7 @@
 
 import paho.mqtt.client as mqtt
 
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -10,6 +11,7 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("mqtt")
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
