2f4f34bd13f0af0a22fff4e8c4936a84f8287615
Evan Carlin <evan@carlin.com>
2017-01-17 17:51:12 -0800

    Basic sender and reciever code

---
+++ mqtt/mqttPublisher.py
@@ -0,0 +1,28 @@
+#If you haven't already done so run
+# $ pip install paho-mqtt
+
+import paho.mqtt.client as mqtt
+import time
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("mqtt")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883, 60)
+
+for x in range(0, 100):
+	print("Sending a message")
+	client.publish(topic="mqtt", payload="Hello", qos=0, retain=False)
+	time.sleep(1)
