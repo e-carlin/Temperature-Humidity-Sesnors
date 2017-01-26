9a242ebc4ac7950c2b1d01648b3ec39c0da970a5
Evan Carlin <evan@carlin.com>
2017-01-26 15:05:41 -0800

    A tester sketch for the DHT22

--- rasPi/readFromSerial.py
+++ rasPi/readFromSerial.py
@@ -14,7 +14,8 @@ import serial, time
 ser = serial.Serial()
 # This port will be different of different machines check correct port with
 # $ python -m serial.tools.list_ports
-ser.port = "/dev/cu.usbserial-DN01Q8E0"
+ser.port = "/dev/cu.usbserial-DN01Q8E0" #For mini-USB cable
+# ser.port = "/dev/cu.usbserial-A5058SOW" #For FTDI cable
 ser.baudrate = 9600
 ser.bytesize = serial.EIGHTBITS #number of bits per bytes
 ser.parity = serial.PARITY_NONE #set parity check: no parity
