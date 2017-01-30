89809b7a82148f414d25ae36f6f0b7ba84df9b96
Evan Carlin <ecarlin@pugetsound.edu>
2017-01-29 16:39:52 -0800

    Removed unecessary files

--- museumWebAppRails/db/migrate/20170130002950_create_readings.rb
+++ museumWebAppRails/db/migrate/20170130002950_create_readings.rb
@@ -1,6 +1,8 @@
 class CreateReadings < ActiveRecord::Migration[5.0]
   def change
     create_table :readings do |t|
+    	t.integer :temperature, null: false
+    	t.integer :humidity, null: false
 
       t.timestamps
     end
--- museumWebAppRails/db/schema.rb
+++ museumWebAppRails/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123220923) do
+ActiveRecord::Schema.define(version: 20170130002950) do
+
+  create_table "readings", force: :cascade do |t|
+    t.integer  "temperature", null: false
+    t.integer  "humidity",    null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
