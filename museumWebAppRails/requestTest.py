3afa8c67105f96affa9681164348c3e0c271b3de
Evan Carlin <evan@carlin.com>
2017-02-14 17:11:46 -0800

    Requests sent to AWSgit status

--- museumWebAppRails/requestTest.py
+++ museumWebAppRails/requestTest.py
@@ -1,6 +1,6 @@
 import requests
-r = requests.post('http://localhost:3000/api/v1/readings', data = 
-	{'temp': '22', 'hum' : '50', 'timeStamp' : '2017-02-15 00:46:34.521341'})
+#r = requests.post('http://localhost:3000/api/v1/readings', data = {'temp': '22', 'hum' : '50', 'timeStamp' : '2017-02-15 00:46:34.521341'})
+r = requests.post('http://ec2-54-202-217-172.us-west-2.compute.amazonaws.com/api/v1/readings', data = {'temp': '22', 'hum' : '50', 'timeStamp' : '2017-02-15 00:46:34.521341'})
 print r
 print r.content
 
