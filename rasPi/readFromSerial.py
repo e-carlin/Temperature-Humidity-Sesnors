4b1fc365272162d0f6551a175a0c5ad83a24745e
Evan Carlin <evan@carlin.com>
2017-02-14 17:22:50 -0800

    Almost ready for PI

--- rasPi/readFromSerial.py
+++ rasPi/readFromSerial.py
@@ -3,7 +3,7 @@
 # A simple script to read incoming data from a usb port
 # and print it to the console
 
-import serial, time
+import serial, time, requests
 from datetime import datetime
 #initialization and open the port
 
@@ -15,9 +15,9 @@ from datetime import datetime
 ser = serial.Serial()
 # This port will be different of different machines check correct port with
 # $ python -m serial.tools.list_ports
-#ser.port = "/dev/cu.usbserial-DN01Q8E0" #For mini-USB cable
+ser.port = "/dev/cu.usbserial-DN01Q8E0" #For mini-USB cable
 #ser.port = "/dev/cu.usbserial-A5058SOW" #For FTDI cable
-ser.port = "/dev/ttyUSB0" #For raspi mini-usb cable
+# ser.port = "/dev/ttyUSB0" #For raspi mini-usb cable
 ser.baudrate = 9600
 ser.bytesize = serial.EIGHTBITS #number of bits per bytes
 ser.parity = serial.PARITY_NONE #set parity check: no parity
@@ -51,15 +51,16 @@ if ser.isOpen():
             response = ser.readline()
             #Only print responses with an actual message in them
             if response != "":
-		f = open("test.txt", "a+")
                 #TODO: Strip \n from response
                 dateString = '%Y/%m/%d %H:%M:%S'
                 dateString = str(datetime.now())
                 response = response.rstrip()
                 response += "\"timeStamp\" : " + dateString + " }"
-		f.write(response+"\n")
-		f.close()
                 print(response)
+                r = requests.post('http://ec2-54-202-217-172.us-west-2.compute.amazonaws.com/api/v1/readings', 
+                    data = response)
+                print r
+                print r.content
 
 
             
