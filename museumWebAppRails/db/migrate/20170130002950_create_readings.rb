c7eb0a415f6c25a5afdff51164461c5fc29cd5bd
Evan Carlin <evan@carlin.com>
2017-02-14 17:01:03 -0800

    Added reading recorded_at to db and readings view

--- museumWebAppRails/db/migrate/20170130002950_create_readings.rb
+++ museumWebAppRails/db/migrate/20170130002950_create_readings.rb
@@ -3,6 +3,7 @@ class CreateReadings < ActiveRecord::Migration[5.0]
     create_table :readings do |t|
     	t.integer :temperature, null: false
     	t.integer :humidity, null: false
+    	t.datetime :recorded_at, null: false
 
       t.timestamps
     end
--- museumWebAppRails/requestTest.py
+++ museumWebAppRails/requestTest.py
@@ -1,5 +1,7 @@
 import requests
 r = requests.post('http://localhost:3000/api/v1/readings', data = 
-	{'temp': '22', 'hum' : '50'})
+	{'temp': '22', 'hum' : '50', 'timeStamp' : '2017-02-15 00:46:34.521341'})
 print r
 print r.content
+
+#{ "temp" : 50.36, "hum" : 46.50, "sID" : 16, "volt" : 3319,"timeStamp" : 2017-02-15 00:46:34.521341 }
