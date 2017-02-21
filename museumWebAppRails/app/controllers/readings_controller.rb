5668c24ad0b034fa6b0de51f86ed9322e114ad5a
Evan Carlin <evan@carlin.com>
2017-02-21 13:31:24 -0800

    Added .to_csv and .to_xls

--- museumWebAppRails/app/controllers/readings_controller.rb
+++ museumWebAppRails/app/controllers/readings_controller.rb
@@ -5,6 +5,11 @@ class ReadingsController < ApplicationController
   # GET /readings.json
   def index
     @readings = Reading.all
+    respond_to do |format|
+      format.html
+      format.csv { send_data @readings.to_csv }
+      format.xls { send_data @readings.to_csv(col_sep: "\t") }
+    end
   end
 
   # GET /readings/1
--- museumWebAppRails/app/models/reading.rb
+++ museumWebAppRails/app/models/reading.rb
@@ -1,2 +1,10 @@
 class Reading < ApplicationRecord
+	def self.to_csv(options = {})
+	  CSV.generate(options) do |csv|
+	    csv << column_names
+	    all.each do |reading|
+	      csv << reading.attributes.values_at(*column_names)
+	    end
+	  end
+	end
 end
