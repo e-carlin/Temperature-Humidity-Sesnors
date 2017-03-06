aa34acd57ad88b20f09ec6f16f6718d16101b96e
Evan Carlin <evan@carlin.com>
2017-03-05 18:09:56 -0800

    Added a little info for building values in controllers instead of views

--- museumWebAppRails/app/controllers/graphs_controller.rb
+++ museumWebAppRails/app/controllers/graphs_controller.rb
@@ -1,6 +1,12 @@
 class GraphsController < ApplicationController
 	def show
-		render template: "pages/#{params[:page]}"
+		#render template: "pages/#{params[:page]}" #TOOD: Do we need this? we don't have a show view...
+	end
+
+	def index
+		@someValue = "I could be a list, or an int, or anything..."
+		@anotherValue = "I'm another value and I can be anything too."
+		#We should build the arrays there in the controller and then pass them to the view.
 	end
 
 
