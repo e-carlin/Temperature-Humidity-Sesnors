8114a6bfcc79a6d42b2da9469465ebd833e39d8d
Evan Carlin <evan@carlin.com>
2017-03-07 20:14:34 -0800

    Cleaned up deploy script

--- museumWebAppRails/deploy.sh
+++ museumWebAppRails/deploy.sh
@@ -1,14 +1,17 @@
-echo "cd to /var/www/museum_monitoring_sensors"
+echo "****** cd to /var/www/museum_monitoring_sensors"
 cd /var/www/museum_monitoring_sensors
-echo "Removing any non saved changes"
+echo "****** Removing any non saved changes"
 sudo git checkout .
-echo "Pulling the latest code"
+echo "****** Pulling the latest code"
 sudo git pull
-echo "cd into app"
+echo "****** cd into app"
 cd museumWebAppRails
-echo "bundle install"
-bundle install --deployment --without development test
-echo "getting secret key"
-bundle exec rake secret
-echo "Please place the secret key in cofig/secrets.yml in place of <%= ENV[SECRET_KEY_BASE] %>"
-echo "Once you have done this run passenger-config restart-app $(pwd)"
+echo "****** bundle install"
+sudo bundle install --deployment --without development test
+echo "****** assets compile"
+bundle exec rake assets:precompile db:migrate RAILS_ENV=production
+echo ""
+echo "****** Getting secret key"
+sudo bundle exec rake secret
+echo "****** Please place the secret key in config/secrets.yml in place of <%= ENV[SECRET_KEY_BASE] %>"
+echo "****** Once you have done this run: sudo passenger-config restart-app $(pwd)"
