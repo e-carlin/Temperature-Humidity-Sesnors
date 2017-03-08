eba3cbd8105d2b5992a159b76e500f287c69b0cd
Evan Carlin <evan@carlin.com>
2017-03-07 20:47:12 -0800

    Removed sudo

--- museumWebAppRails/deploy.sh
+++ museumWebAppRails/deploy.sh
@@ -7,11 +7,11 @@ sudo git pull
 echo "****** cd into app"
 cd museumWebAppRails
 echo "****** bundle install"
-sudo bundle install --deployment --without development test
+bundle install --deployment --without development test
 echo "****** assets compile"
 bundle exec rake assets:precompile db:migrate RAILS_ENV=production
 echo ""
 echo "****** Getting secret key"
-sudo bundle exec rake secret
+bundle exec rake secret
 echo "****** Please place the secret key in config/secrets.yml in place of <%= ENV[SECRET_KEY_BASE] %>"
 echo "****** Once you have done this run: sudo passenger-config restart-app $(pwd)"
