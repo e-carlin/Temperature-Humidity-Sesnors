650d5bb7e84638f6a8b10d9541cf8c3a990aaddd
Mark Gilbert <mgilbert311@gmail.com>
2017-03-09 09:09:14 -0800

    Adding changes to production.rb

--- museumWebAppRails/config/environments/production.rb
+++ museumWebAppRails/config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
  config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
+  :domain               => 'gmail.com',
   :user_name            => 'lelooskasensorwebsite@gmail.com',
   :password             => 'lelooskaCapstone',
   :authentication       => "plain",
