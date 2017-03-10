1a8dae25a78357576b37dc136d6ce024dd09c2a1
Mark Gilbert <mgilbert311@gmail.com>
2017-03-10 09:03:18 -0800

    Small changes

--- museumWebAppRails/config/initializers/clearance.rb
+++ museumWebAppRails/config/initializers/clearance.rb
@@ -1,4 +1,6 @@
 Clearance.configure do |config|
   config.routes = false
-  config.mailer_sender = "reply@example.com"
+  config.mailer_sender = "LelooskaSensorWebsite@gmail.com"
+
+  #config.mailer_sender = "reply@example.com"
 end
