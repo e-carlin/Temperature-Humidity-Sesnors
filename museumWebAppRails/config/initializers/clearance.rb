d12998eaa015cf6959ccecc45651b4095a62b297
Mark Gilbert <mgilbert311@gmail.com>
2017-03-10 09:44:54 -0800

    Fixing small error

--- museumWebAppRails/config/initializers/clearance.rb
+++ museumWebAppRails/config/initializers/clearance.rb
@@ -9,11 +9,11 @@ Clearance.configure do |config|
   config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
   config.cookie_name = "remember_token"
   config.cookie_path = "/"
-  config.routes = true
+  config.routes = false
   config.httponly = false
   config.mailer_sender = "LelooskaSensorWebsite@gmail.com"
   config.password_strategy = Clearance::PasswordStrategies::BCrypt
-  config.redirect_url = "ec2-54-202-217-172.us-west-2.compute.amazonaws.com"
+  config.redirect_url = "/"
   config.rotate_csrf_on_sign_in = false
   config.secure_cookie = false
   config.sign_in_guards = []
