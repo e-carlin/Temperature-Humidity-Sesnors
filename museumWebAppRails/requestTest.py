e6cb4f77af6cac0071db0aa1cf594045ab08594a
Evan Carlin <evan@carlin.com>
2017-03-21 16:38:54 -0700

    Fixed bad json formatting

--- museumWebAppRails/requestTest.py
+++ museumWebAppRails/requestTest.py
@@ -1,6 +1,11 @@
-import requests
-r = requests.post('http://localhost:3000/api/v1/readings', headers={'Authorization' : 'rasPiAuth..0246'}, data = {"error" : "this is an error", "temp" : '58.82', "hum" : '50.50', "volt" : 3271,  "node_id" : 8, "timeStamp"  : "2017-03-06 14:44:13.193749-08:00"}
+import requests, json
+url = 'http://localhost:3000/api/v1/readings'
+info = json.loads('{"error" : "this is an error"}')
+r = requests.post(url, 
+	headers={'Authorization' : 'rasPiAuth..0246', 'Content-type': 'application/json'}, 
+	data = json.dumps(info)
 )
+
 # r = requests.post('http://localhost:3000/api/v1/readings', data = {'err' : 'an error'})
 # r = requests.post('http://ec2-54-202-217-172.us-west-2.compute.amazonaws.com/api/v1/readings', data = {'temp': '22', 'hum' : '50', 'timeStamp' : '2017-02-15 00:46:34.521341'})
 print r.content
