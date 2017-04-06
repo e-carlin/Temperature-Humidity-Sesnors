48ee8dfecf170af7dfc3292a6a25a4037be79c63
Evan Carlin <evan@carlin.com>
2017-04-06 10:44:32 -0700

    added info for setting up brad server

--- museumWebAppRails/requestTest.py
+++ museumWebAppRails/requestTest.py
@@ -1,9 +1,15 @@
 import requests, json
-url = 'http://localhost:3000/api/v1/readings'
-info = json.loads('{"error" : "this is an error"}')
+# url = 'http://localhost:3000/api/v1/readings'
+# url = 'http://ec2-54-202-217-172.us-west-2.compute.amazonaws.com/api/v1/readings'
+url = 'http://ec2-52-11-161-114.us-west-2.compute.amazonaws.com/sensors/api/v1/readings'
+# info = json.loads('{"error" : "this is Evan testing the error logs"}')
+info = '{ "node_id" : 2, "temp" : 50.36, "hum" : 46.50, "sID" : 16, "volt" : 3319,"timeStamp" : "2017-02-15 07:46:34.521341" }'
+
 r = requests.post(url, 
 	headers={'Authorization' : 'rasPiAuth..0246', 'Content-type': 'application/json'}, 
-	data = json.dumps(info)
+	# data = json.dumps(info)
+	data = info
+
 )
 
 # r = requests.post('http://localhost:3000/api/v1/readings', data = {'err' : 'an error'})
@@ -11,4 +17,4 @@ r = requests.post(url,
 print r.content
 print r
 
-#{ "temp" : 50.36, "hum" : 46.50, "sID" : 16, "volt" : 3319,"timeStamp" : 2017-02-15 00:46:34.521341 }
+
