e67a01cd98547daa982c262a4dbb096c57e9496b
Evan Carlin <evan@carlin.com>
2017-04-06 10:24:55 -0700

    Added relative to prod.rb

--- museumWebAppRails/config/environments/production.rb
+++ museumWebAppRails/config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.root_directory = '/sensors'
+    config.assets.prefix = '/sensors/assets/'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
