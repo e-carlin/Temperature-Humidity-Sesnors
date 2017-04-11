c9ace817897aa837634d9734dd83084c78489d26
Evan Carlin <evan@carlin.com>
2017-04-10 22:00:56 -0700

    Remove active record set

--- museumWebAppRails/config/application.rb
+++ museumWebAppRails/config/application.rb
@@ -13,6 +13,5 @@ module MuseumWebAppRails
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   	config.time_zone = 'Pacific Time (US & Canada)'
-  	config.active_record.default_timezone = :utc
   end
 end
